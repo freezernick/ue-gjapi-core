6e4c09003d67998560f743b93a599b150d674b2c
FreezerNick <nl@f2p-entertainment.de>
2020-07-03 10:38:49 +0200

    Simplify GameJoltAPI.cpp

--- Source/GameJoltAPI/Private/GameJoltAPI.cpp
+++ Source/GameJoltAPI/Private/GameJoltAPI.cpp
@@ -1,20 +1,13 @@
-// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+// Copyright by Free2Play-Entertainment (2020)
 
 #include "GameJoltAPI.h"
 
-#define LOCTEXT_NAMESPACE "FGameJoltAPIModule"
-
 void FGameJoltAPIModule::StartupModule()
 {
-	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
 }
 
 void FGameJoltAPIModule::ShutdownModule()
 {
-	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
-	// we call this function before unloading the module.
 }
 
-#undef LOCTEXT_NAMESPACE
-	
 IMPLEMENT_MODULE(FGameJoltAPIModule, GameJoltAPI)
