0f29fe722062151414c57fe2cbdcd52e60a0345d
FreezerNick <nl@f2p-entertainment.de>
2020-07-29 11:20:23 +0200

    Add AddScore Node; #32

---
+++ Source/GameJoltAPI/AsyncActions/AddScore.cpp
@@ -0,0 +1,52 @@
+// Copyright by Free2Play-Entertainment (2020)
+
+
+#include "AddScore.h"
+
+UAddScore* UAddScore::AddScore(UObject* WCO, UGameJolt* GJAPI, const FString Score, const int32 ScoreSort, const int32 TableID, const FString Guest, const FString ExtraData)
+{
+    UAddScore* ScoreNode = NewObject<UAddScore>();
+    ScoreNode->WorldContextObject = WCO;
+    ScoreNode->GameJolt = GJAPI;
+    ScoreNode->ScoreString = Score;
+    ScoreNode->Sort = ScoreSort;
+    ScoreNode->Table = TableID;
+    ScoreNode->GuestName = Guest;
+    ScoreNode->ExtraScoreData = ExtraData;
+    return ScoreNode;
+}
+
+void UAddScore::Activate()
+{
+    if(!Super::Validate() || (!GameJolt->bLoggedIn && GuestName == ""))
+    {
+        Failure.Broadcast();
+        return;
+    }
+
+    FScriptDelegate funcDelegate;
+    funcDelegate.BindUFunction(this, "Callback");
+
+    FString BaseURL = "/scores/add/?";
+    if(!GameJolt->bLoggedIn)
+        BaseURL += "&guest=" + GuestName;
+    if(Table != 0)
+        BaseURL += "&table_id=" + FString::FromInt(Table);
+    if(ExtraScoreData != "")
+        BaseURL += "&extra_data=" + ExtraScoreData;
+    BaseURL += "&score=" + ScoreString + "&sort" = FString::FromInt(Sort);
+    FieldData = UJsonFieldData::GetRequest(UGameJolt::CreateURL(BaseURL, GameJolt));
+    FieldData->OnGetResult.AddUnique(funcDelegate);
+}
+
+void UAddScore::Callback(const bool bSuccess, UJsonFieldData* JSON, const EJSONResult Status)
+{
+    Super::Callback(bSuccess, JSON, Status);
+    if(!bResponseValid)
+    {
+        Failure.Broadcast();
+        return;
+    }
+
+    Success.Broadcast();
+}
