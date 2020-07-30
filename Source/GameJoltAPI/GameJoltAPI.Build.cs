945781ba868256771263e3050851d81204c2b942
FreezerNick <nl@f2p-entertainment.de>
2020-07-30 16:53:02 +0200

    Restructure AsyncActions Folder

--- Source/GameJoltAPI/GameJoltAPI.Build.cs
+++ Source/GameJoltAPI/GameJoltAPI.Build.cs
@@ -1,12 +1,20 @@
 // Copyright by Free2Play-Entertainment (2020)
 
 using UnrealBuildTool;
+using System.IO;
 
 public class GameJoltAPI : ModuleRules
 {
     public GameJoltAPI(ReadOnlyTargetRules Target) : base(Target)
     {
         PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PrivateIncludePaths.AddRange(
+            new string[]
+            {
+                Path.Combine(ModuleDirectory, "AsyncActions")
+            }
+        );
 
         PublicDependencyModuleNames.AddRange(
             new string[]
