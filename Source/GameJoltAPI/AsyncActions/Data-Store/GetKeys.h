4083225f6ec9dd6b2ace18909c9125eaf8ee46f1
FreezerNick <nl@f2p-entertainment.de>
2020-07-31 14:08:31 +0200

    Add another missing comment

--- Source/GameJoltAPI/AsyncActions/Data-Store/GetKeys.h
+++ Source/GameJoltAPI/AsyncActions/Data-Store/GetKeys.h
@@ -19,6 +19,11 @@ class GAMEJOLTAPI_API UGetKeys final : public UGameJoltAsyncBase
 
 public:
 
+	/**
+	 * Returns either a list of keys in the game's global data store, or a list of keys in the current user's data store.
+	 * @param Scope If you select 'User', this function will look for keys in a user's data store. If you select 'Global', it will look for keys in the game's global data store.
+	 * @param Pattern (optional) The pattern to apply to the key names in the data store. Placeholder character is '*'
+	 */
 	UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true"))
 	static UGetKeys* GetKeys(UGameJolt* GameJoltAPI, EGJDataStore Scope, const FString Pattern = "");
 
