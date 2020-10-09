e530541e9798d2a6ac8e62f16b2a3252f9969dd9
FreezerNick <nl@f2p-entertainment.de>
2020-10-09 11:47:15 +0200

    Prevent invalid URL errors

--- Source/GameJoltAPI/Private/GameJolt.cpp
+++ Source/GameJoltAPI/Private/GameJolt.cpp
@@ -2,7 +2,6 @@
 
 
 #include "GameJolt.h"
-#include "Misc/Paths.h"
 #include "Misc/SecureHash.h"
 
 void UGameJolt::Initialize(const int32 game_id, const FString private_key)
@@ -29,7 +28,7 @@ void UGameJolt::Logout()
 
 FString UGameJolt::CreateURL(const FString URL, bool AppendUserInfo)
 {
-    UGameJolt* GameJolt = UGameJolt::Get(); 
-    FString BaseURL = FPaths::Combine(FString(GameJolt->Server + GameJolt->Version), URL) + "&game_id=" + FString::FromInt(GameJolt->GameID) + ((GameJolt->bLoggedIn && AppendUserInfo) ? "&username=" + GameJolt->UserName + "&user_token=" + GameJolt->UserToken : "");
+    UGameJolt* GameJolt = UGameJolt::Get();
+    FString BaseURL = "https://api.gamejolt.com/api/game/v1_2/" + URL + "&game_id=" + FString::FromInt(GameJolt->GameID) + ((GameJolt->bLoggedIn && AppendUserInfo) ? "&username=" + GameJolt->UserName + "&user_token=" + GameJolt->UserToken : "");
     return (BaseURL + "&signature=" + FMD5::HashAnsiString(*(BaseURL + GameJolt->PrivateKey)));
 }
