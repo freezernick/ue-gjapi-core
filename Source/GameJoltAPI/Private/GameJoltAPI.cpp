f1b66fa6e8a531d66a6d400db3375032e917517f
Tayzar Linn <sueki.tayzar@gmail.com>
2023-02-17 16:59:12 -0500

    feat: add GameJoltAPI log category

--- Source/GameJoltAPI/Private/GameJoltAPI.cpp
+++ Source/GameJoltAPI/Private/GameJoltAPI.cpp
@@ -2,6 +2,10 @@
 
 #include "GameJoltAPI.h"
 
+DEFINE_LOG_CATEGORY(GameJoltAPI);
+
+#define LOCTEXT_NAMESPACE "GameJoltAPI"
+
 void FGameJoltAPIModule::StartupModule()
 {
 }
@@ -9,5 +13,7 @@ void FGameJoltAPIModule::StartupModule()
 void FGameJoltAPIModule::ShutdownModule()
 {
 }
+
+#undef LOCTEXT_NAMESPACE
 
 IMPLEMENT_MODULE(FGameJoltAPIModule, GameJoltAPI)
