741335c0ce3d411a1c45aa1fdf8a35cfdc1e71bd
Tayzar Linn <sueki.tayzar@gmail.com>
2023-02-17 16:59:12 -0500

    Add Settings & Logging

--- Source/GameJoltAPI/Private/GameJoltAPI.cpp
+++ Source/GameJoltAPI/Private/GameJoltAPI.cpp
@@ -1,13 +1,39 @@
 // Copyright by Nick Lamprecht (2020-2021)
 
 #include "GameJoltAPI.h"
+#include "GameJoltSettings.h"
+#include "Developer/Settings/Public/ISettingsModule.h"
+
+DEFINE_LOG_CATEGORY(GameJoltAPI);
+
+#define LOCTEXT_NAMESPACE "GameJoltAPI"
 
 void FGameJoltAPIModule::StartupModule()
 {
+	if (ISettingsModule* SettingsModule = FModuleManager::GetModulePtr<ISettingsModule>("Settings"))
+	{
+		UGameJoltSettings* PluginSettings = GetMutableDefault<UGameJoltSettings>();
+
+		SettingsModule->RegisterSettings("Project", "Plugins", "GameJoltAPI",
+			LOCTEXT("RuntimeSettingsName", "Game Jolt"),
+			LOCTEXT("RuntimeSettingsDescription", "Configure the Game Jolt API Plugin"),
+			PluginSettings
+		);
+	}
+
+	UE_LOG(GameJoltAPI, Log, TEXT("Game Jolt API module has been loaded!"));
 }
 
 void FGameJoltAPIModule::ShutdownModule()
 {
+	if (ISettingsModule* SettingsModule = FModuleManager::GetModulePtr<ISettingsModule>("Settings"))
+	{
+		SettingsModule->UnregisterSettings("Project", "Plugins", "GameJoltAPI");
+	}
+
+	UE_LOG(GameJoltAPI, Log, TEXT("Game Jolt API module has been unloaded!"));
 }
+
+#undef LOCTEXT_NAMESPACE
 
 IMPLEMENT_MODULE(FGameJoltAPIModule, GameJoltAPI)
--- Source/GameJoltAPI/Public/GameJoltAPI.h
+++ Source/GameJoltAPI/Public/GameJoltAPI.h
@@ -5,6 +5,8 @@
 #include "CoreMinimal.h"
 #include "Modules/ModuleManager.h"
 
+DECLARE_LOG_CATEGORY_EXTERN(GameJoltAPI, All, All)
+
 class FGameJoltAPIModule : public IModuleInterface
 {
 public:
